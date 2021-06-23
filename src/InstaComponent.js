10ba17f0e3a1d7ab73908a23fb6e801f9dde68e0
Jessica Brand <jeshbrand@gmail.com>
2021-06-23 17:00:27 +0100

    display now sensitive to the loggedIn state on the InstaComponent

--- src/InstaComponent.js
+++ src/InstaComponent.js
@@ -8,57 +8,89 @@ import PhotoComponent from './PhotoComponent'
 export default class InstaComponent extends Component {
 
   constructor(props){
-    super(props)
+    super(props);
+    this.state = {
+      loggedIn: false,
+    }
   }
 
   render(){
-    return (
-      <div className="App">
+    if (this.state.loggedIn == false) {
 
-        <header className="App-header">
-          <div id='boilerplate'>
-            <img src={logo} className="App-logo" alt="logo" />
-            <p> Edit <code>src/InstaComponent.js</code> and save to reload. </p>
-            <a className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
+      console.log('NOT logged in')
+
+      return (
+        <div className="App">
+
+          <header className="App-header">
+            <div id='boilerplate'>
+              <img src={logo} className="App-logo" alt="logo" />
+              <p> Edit <code>src/InstaComponent.js</code> and save to reload. </p>
+              <a className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </div>
+          </header>
+
+          <div className="signup">
+            <h3> this should be visible BEFORE signup OR login</h3>
+            <form>
+              <input type='text' id='first_name' placeholder='first name'></input> <br />
+              <input type='text' id='email' placeholder='email address'></input> <br />
+              <input type='text' id='password' placeholder='password'></input> <br />
+              <input type='submit' value='sign up!'></input>
+            </form>
           </div>
-        </header>
 
-        <div className="signup">
-          <h3> this should be visible BEFORE signup OR login</h3>
-          <form>
-            <input type='text' id='first_name' placeholder='first name'></input> <br />
+          <div className="login">
+            <h3> this should be visible BEFORE login</h3>
+            <form>
             <input type='text' id='email' placeholder='email address'></input> <br />
             <input type='text' id='password' placeholder='password'></input> <br />
-            <input type='submit' value='sign up!'></input>
-          </form>
+            <input type='submit' value='log in!'></input>
+            </form>
+          </div>
+
         </div>
+      )
+    } else {
 
-        <div className="login">
-          <h3> this should be visible BEFORE login</h3>
-          <form>
-          <input type='text' id='email' placeholder='email address'></input> <br />
-          <input type='text' id='password' placeholder='password'></input> <br />
-          <input type='submit' value='log in!'></input>
-          </form>
+      console.log(' YES logged in')
+
+      return (
+
+        <div className="App">
+
+          <header className="App-header">
+            <div id='boilerplate'>
+              <img src={logo} className="App-logo" alt="logo" />
+              <p> Edit <code>src/InstaComponent.js</code> and save to reload. </p>
+              <a className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </div>
+          </header>
+
+          <div className="feed">
+          <h3> this should be visible AFTER login </h3>
+            <PostComponent
+            caption="I'm a caption [prop]"
+            photo=<PhotoComponent source='default.png' />
+            comments=<CommentComponent content="I'm a comment component. I belong to this photo! ^ "/>
+            />
+          </div>
+
         </div>
+      )
+    }
 
-        <div className="feed">
-        <h3> this should be visible AFTER login </h3>
-          <PostComponent
-          caption="I'm a caption [prop]"
-          photo=<PhotoComponent source='default.png' />
-          comments=<CommentComponent content="I'm a comment component. I belong to this photo! ^ "/>
-          />
-        </div>
-
-
-      </div>
-    )
   }
 }
